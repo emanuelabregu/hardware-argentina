dad0e4aec0694a10162d9b10a5b0305198932a5a
Julio Emanuel Abregu <emanuelabregu17@gmail.com>
2022-07-05 14:09:31 -0300

    actualizacion shop y calculadora de cuotas

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,7 @@ numero2 = parseFloat(numero2)
 let resultado = numero1 + numero2
 alert("El resultado es: " + resultado)
 */
+/*
 let usuario = prompt ("Hola! Ingresa tu usuario");
 let clave = prompt ("Ahora ingresa tu clave");
 if(usuario == "emanuel" && clave == "12345")
@@ -59,3 +60,30 @@ if(usuario == "emanuel" && clave == "12345")
                 };
         };
     };
+    *
+*/
+
+
+
+//
+let precio = 0 
+let cuotas = 0
+let resultado = 0
+precio = prompt("ingrese un valor a cuotificar")
+cuotas = prompt("por favor ingrese cantidad de cuotas: 3,6 o 12")
+precio = parseFloat(precio)
+cuotas = parseFloat(cuotas)
+let intereses6= ((precio * 25) / 100);
+let intereses12 = ((precio * 50) / 100);
+if(cuotas == 3){
+    resultado = precio / cuotas;
+    alert("Usted pagara por mes SIN INTERES, 3 cuotas de: $" + resultado);
+}
+    else if (cuotas == 6){
+            resultado = ((precio + intereses6) / cuotas) ;
+        alert("Usted pagara por mes CON INTERES total de: " + intereses6 +", 6 cuotas de: $"+ resultado);
+}
+    else if (cuotas == 12){
+        resultado = (precio / cuotas) + intereses12;
+        alert("Usted pagara por mes CON INTERES total de: " + intereses12 +", 12 cuotas de: $"+ resultado);
+};
